2b981ab4748261c98cb60f23f5c1a6d3d3b195cd
Michele Dalle Rive <dallem@usi.ch>
2023-09-17 00:59:10 +0200

    Install Vuetify

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,4 +3,13 @@ import App from "./App.vue";
 import router from "./router";
 import store from "./store";
 
-createApp(App).use(store).use(router).mount("#app");
+import * as components from "vuetify/components";
+import * as directives from "vuetify/directives";
+import { createVuetify } from "vuetify";
+
+const vuetify = createVuetify({
+  components,
+  directives,
+});
+
+createApp(App).use(store).use(router).use(vuetify).mount("#app");
