bca30453505ae72b761b00075c29e1719106c263
Loredana De Filippo <defill@usi.ch>
2023-09-17 05:19:22 +0200

    Updated the custom scan

--- frontend/src/views/CustomScan.vue
+++ frontend/src/views/CustomScan.vue
@@ -2,25 +2,24 @@
   <div>
     <div class="d-flex flex-column justify-content-center align-items-center">
       <div class="d-flex flex-column align-center">
-        <v-img :src="require('@/assets/spider.png')" alt="hi" width="160" height="160"/>
+        <v-img
+          :src="require('@/assets/spider.png')"
+          alt="hi"
+          width="160"
+          height="160"
+        />
         <h1 class="my-8">One-gO</h1>
-        <p class="">
-          Please specify the Directory you would like to scan
-        </p>
-
-        <div class="d-flex flex-column align-center mt-8">
-          <v-text-field
-            v-model="directory"
-            label="Directory"
-            outlined
-            dense
-            class="w-100"
-          />
-
+        <p class="">Please specify the Directory you would like to scan</p>
+        <div class="d-flex w-100 justify-center my-6">
+          <v-col cols="12" sm="6" md="4">
+            <v-text-field
+              label="Directory"
+              variant="outlined"
+            ></v-text-field>
+          </v-col>
         </div>
-
       </div>
-      <div class="d-flex flex-column align-center mt-8">
+      <div class="d-flex flex-column align-center">
         <ScanButton />
       </div>
     </div>
@@ -28,11 +27,12 @@
 </template>
 
 <script>
+import ScanButton from "@/components/ScanButton.vue";
+
 export default {
-  name: "CustomScan"
+  name: "CustomScan",
+  components: { ScanButton }
 };
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
